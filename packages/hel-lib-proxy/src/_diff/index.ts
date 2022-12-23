2ac9f166ce0aab45052133b26269cea6528c8ea3
fantasticsoul <zhongzhengkai@gmail.com>
2022-12-23 17:25:19 +0800

    feat: bump to 3.10.0

--- packages/hel-lib-proxy/src/_diff/index.ts
+++ packages/hel-lib-proxy/src/_diff/index.ts
@@ -1,4 +1,4 @@
-export const VER = '3.9.6';
+export const VER = '3.10.2';
 
 export function getDefaultPlatform(inputPlatform?: string) {
   return inputPlatform || 'unpkg';
--- packages/hel-micro/src/_diff/base.ts
+++ packages/hel-micro/src/_diff/base.ts
@@ -1,1 +1,1 @@
-export const VER = '3.10.1';
+export const VER = '3.10.3';
