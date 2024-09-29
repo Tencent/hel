65617b589fcafe4215895ec8af561257d93b0bd2
fancyzhong <fancyzhong@tencent.com>
2024-09-29 18:54:28 +0800

    build(react-4.9.8): issue 110, return Error Component

--- packages/hel-micro-react/src/components/RemoteCompRender/index.tsx
+++ packages/hel-micro-react/src/components/RemoteCompRender/index.tsx
@@ -24,7 +24,7 @@ export function useRemoteCompRender(props: IRemoteCompRenderConfig) {
   }
   if (errMsg) {
     controlOptions.failCb?.(new Error(errMsg));
-    return { RemoteCompRender: () => RemoteModule, isReady: false, err: new Error(errMsg) };
+    return { RemoteCompRender: RemoteModule, isReady: false, err: new Error(errMsg) };
   }
 
   const compInfo = { Comp: RemoteModule, styleStr, styleUrlList };
