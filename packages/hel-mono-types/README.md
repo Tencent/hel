df913113b48167ffe08be956be3085df7071ff1a
fancyzhong <fancyzhong@tencent.com>
2025-05-12 12:17:39 +0800

    feat: add hel-mono-types

---
+++ packages/hel-mono-types/README.md
@@ -0,0 +1,3 @@
+# hel-mono-types
+
+hel-mono 架构共享的 type 文件
---
+++ packages/hel-mono-types/index.d.ts
@@ -0,0 +1,92 @@
+/**
+ * @description: hel-mono 依赖的类型
+ */
+
+/**
+ * hel应用（模块）配置
+ */
+export interface IHelModConf {
+  /**
+   * hel应用（模块）对应的hel组名
+   */
+  appGroupName: string;
+  /**
+   * 测试、线上对应的hel应用名（模块名）
+   */
+  appNames: {
+    test: string;
+    /** prod 未配置时，和 appGroupName 保持一致 */
+    prod?: string;
+  };
+  /**
+   * default: IMonoDevInfo.devHostname
+   * 当前hel模块本地联调时的域名
+   */
+  devHostname?: string;
+  /**
+   * hel应用（模块）所属平台
+   * default: 'unpkg'
+   */
+  platform?: string;
+}
+
+export interface IMonoAppConf {
+  /**
+   * 应用启动端口
+   */
+  port: number;
+  /**
+   * 应用相对路径别名
+   */
+  alias?: string;
+  /**
+   * hel应用（模块）相关配置
+   */
+  hel?: IHelModConf;
+}
+
+export interface IEnsuredMonoAppConf {
+  port: number;
+  hel: IHelModConf;
+  /**
+   * default: IMonoInjectedDevInfo.devHostname
+   * 当前hel模块本地联调时的域名
+   */
+  devHostname?: string;
+}
+
+export type MonoAppConfs = Record<string, IMonoAppConf>;
+
+/**
+ * hel-mono 大仓开发信息
+ */
+export interface IMonoDevInfo {
+  /**
+   * externals 映射配置
+   */
+  appExternals: Record<string, string>,
+  /**
+   * 各应用（或子模块）的大仓开发配置
+   */
+  appConfs: MonoAppConfs;
+  /** default: ['apps'], 放置应用的目录名列表 */
+  appsDirs?: string;
+  /** default: ['packages'], 放置子模块的目录名列表 */
+  subModDirs?: string;
+  /**
+   * default: 'http://localhost'
+   * 所有hel模块本地联调时的域名
+   */
+  devHostname?: string;
+}
+
+/**
+ * 通过脚本裁剪 dev-info 配置后，注入到应用代码里的开发信息
+ */
+export interface IMonoInjectedDevInfo {
+  /**
+   * 各应用（或子模块）的大仓开发配置
+   */
+  appConfs: Record<string, IEnsuredMonoAppConf>;
+  devHostname: string;
+}
