6492b7e95190109ed49455f139eef5cfc4c5e675
fancyzhong <fancyzhong@tencent.com>
2025-07-19 20:34:41 +0800

    feat: add port file

---
+++ packages/hel-mono-helper/src/util/port.js
@@ -0,0 +1,18 @@
+/** @typedef {import('hel-mono-types').IMonoDevInfo} IMonoDevInfo*/
+
+exports.getNewPort = function (/** @type {IMonoDevInfo} */ devInfo) {
+  const { appConfs } = devInfo;
+  let maxPort = 0;
+  Object.keys(appConfs).forEach((key) => {
+    const port = appConfs[key].port || 0;
+    if (port > maxPort) {
+      maxPort = port;
+    }
+  });
+
+  if (!maxPort) {
+    return 3000;
+  }
+
+  return maxPort + 1;
+};
