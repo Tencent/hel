ba1f171be54a1aad3a476b05c91a706bd071110c
fancyzhong <fancyzhong@tencent.com>
2025-08-24 11:24:38 +0800

    feat: add mono-dir

---
+++ packages/hel-mono-helper/src/util/mono-dir.js
@@ -0,0 +1,37 @@
+const { lastNItem } = require('./arr');
+const { getMonoRootInfo } = require('./root-info');
+const { getDevInfoDirs } = require('./dev-info');
+const { pickOneDir } = require('./mono-path');
+
+function inferDirFromDevInfo(devInfo, allowEmptyDir) {
+  //  执行 pnpm start 时，从目录结构里挑出一个并返回
+  const { appsDirs, subModDirs } = getDevInfoDirs(devInfo);
+  const { monoRoot } = getMonoRootInfo();
+  let targetDirName = pickOneDir(monoRoot, appsDirs);
+
+  if (!targetDirName) {
+    targetDirName = pickOneDir(monoRoot, subModDirs);
+  }
+
+  if (!allowEmptyDir && !targetDirName) {
+    throw new Error('current hel-mono project is empty, please execute "pnpm start .create my-app" to add one app');
+  }
+
+  return targetDirName;
+}
+
+function inferDirFromArgv2ndItem(devInfo, allowEmptyDir) {
+  const argv = process.argv;
+  const mayDirName = lastNItem(argv, 2);
+
+  if (mayDirName) {
+    return mayDirName;
+  }
+
+  return inferDirFromDevInfo(devInfo, allowEmptyDir);
+}
+
+module.exports = {
+  inferDirFromArgv2ndItem,
+  inferDirFromDevInfo,
+};
