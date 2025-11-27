568556d505bd537c70099f4da38590adf919652c
grx <bjguoruixiang@126.com>
2025-11-27 12:09:53 +0800

    feat: add report

--- helpack/hm-browser-user/src/App.tsx
+++ helpack/hm-browser-user/src/App.tsx
@@ -1,3 +1,4 @@
+import { hello } from '@hel-demo/mono-libs';
 import './App.css';
 import logo from './logo.svg';
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>{hello()}</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
