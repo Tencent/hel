0e9386379c7e8f1e08842459ccf847f9ccf39a3b
fancyzhong <fancyzhong@tencent.com>
2025-12-15 15:24:54 +0800

    chore: fmt

--- helpack/server/src/controllers/local/index.ts
+++ helpack/server/src/controllers/local/index.ts
@@ -1,4 +1,4 @@
 export * as app from './app';
 export * as appVersion from './appVersion';
-export * as hmnService from './hmnService';
 export * as hmn from './hmn';
+export * as hmnService from './hmnService';
