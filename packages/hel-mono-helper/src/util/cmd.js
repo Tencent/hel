0a1008894afaa7f9f8e4f71c7739fac3f000b7b4
fancyzhong <fancyzhong@tencent.com>
2025-12-29 16:46:27 +0800

    feat(mono-helper): add devDir

--- packages/hel-mono-helper/src/util/cmd.js
+++ packages/hel-mono-helper/src/util/cmd.js
@@ -1,15 +1,35 @@
 const shell = require('shelljs');
 const path = require('path');
 const { getMonoRootInfo } = require('./rootInfo');
+const { getRawMonoJson } = require('./monoJson');
 
 function runAppAction(pkgName, scriptCmdKey) {
   const cmd = `pnpm --filter ${pkgName} run ${scriptCmdKey}`;
   shell.exec(cmd);
 }
 
-function createApp(pkgName, tplType = 'react-app', belongTo) {
+function getDevDirPath() {
+  let devDirPath = 'dev';
+  const monoJson = getRawMonoJson();
+  if (monoJson) {
+    devDirPath = monoJson.devDir || 'dev';
+  }
+  if (devDirPath.startsWith('/')) {
+    return devDirPath;
+  }
+
   const { monoRoot } = getMonoRootInfo();
-  const startScript = path.join(monoRoot, './dev/root-scripts/executeStart');
+  let dirSeg = devDirPath;
+  if (!dirSeg.startsWith('.')) {
+    dirSeg = `./${dirSeg}`;
+  }
+
+  return path.join(monoRoot, dirSeg);
+}
+
+function createApp(pkgName, tplType = 'react-app', belongTo) {
+  const devDirPath = getDevDirPath();
+  const startScript = path.join(devDirPath, './root-scripts/executeStart');
   let cmd = `node ${startScript} .create ${pkgName} -t ${tplType}`;
   if (belongTo) {
     cmd += ` -d ${belongTo}`;
